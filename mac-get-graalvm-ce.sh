8186be08fe33a0664006dea3dfe9eeb83f640b75
Edwin Derks <edwin.derks@ordina.nl>
2019-03-08 14:06:19 +0100

    Adding Polyglot example

--- mac-get-graalvm-ce.sh
+++ mac-get-graalvm-ce.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Installing
+echo "Installing GraalVM in current folder."
+echo "This can take a minute..."
 curl -L -s https://github.com/oracle/graal/releases/download/vm-1.0.0-rc13/graalvm-ce-1.0.0-rc13-macos-amd64.tar.gz |tar xz
 
---
+++ src/main/java/nl/ordina/holygraalvm/examples/polyglot/PolyglotGraalVM.java
@@ -0,0 +1,8 @@
+package nl.ordina.holygraalvm.examples.polyglot;
+
+public class PolyglotGraalVM {
+
+    public static void main(String... args) {
+        System.out.println("Hello GraalVM");
+    }
+}
